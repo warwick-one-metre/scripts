f2d7957f9dc20323b973e3eb7c2a1860624d2d67
Paul Chote <pchote@users.noreply.github.com>
2015-08-31 14:25:07 +0100

    Update acquire script to match latest test version.

--- indi/acquire.sh
+++ indi/acquire.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Acquire a run of frames using a single camera
 
-basedir=/home/testnuc/OBS_DATA/20150822/test-
+basedir=/home/testnuc/OBS_DATA/20150831/test-
 cam=andor0
-exp_count=20
-exp_start=0
+exp_count=1
+#exp_start=0
 exp_time=1
 exp_shutter_close=1
 
@@ -17,10 +17,20 @@ fi
 
 setINDI ${cam}.image_exposure.time=${exp_time}
 setINDI ${cam}.archive.base_name=${basedir}
-setINDI ${cam}.image.number=${exp_start}
+# setINDI ${cam}.image.number=${exp_start}
+
+# To acquire multiple frames without the bash loop
+# setINDI ${cam}.acquire.single=Off
+# setINDI ${cam}.acquire.multi=On
+# setINDI ${cam}.sequence.run=On
+
+# Acquiring individual frames in a loop so we can
+# process them as they come in
 setINDI ${cam}.acquire.single=On
 
-echo "here"
+setINDI ${cam}.fits_script.file="ssh saft@192.168.0.101 getshm"
+setINDI ${cam}.fits_script.run=On
+
 for i in `seq 1 1 ${exp_count}`; do
     setINDI ${cam}.sequence.run=On
     echo "start"
@@ -31,7 +41,9 @@ for i in `seq 1 1 ${exp_count}`; do
         sleep 1
         run=`getINDI ${cam}.sequence.run | cut -f 2,2 -d "=" | grep -c "On"`
     done
-    saved=`getINDI andor0.file.name | cut -f 2,2 -d "="`
+    saved=`getINDI ${cam}.file.name | cut -f 2,2 -d "="`
     tsreduce preview ${saved} Online_Preview
+#    ./guide.sh
+#    ./reduce.sh
     echo "Saved " ${saved}
 done
